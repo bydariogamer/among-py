632d996f13b21bdb284603f066161148232800c7
bydariogamer <62303655+bydariogamer@users.noreply.github.com>
2021-02-09 22:00:39 +0100

    added and updated many files

--- textbox.py
+++ textbox.py
@@ -1,17 +1,24 @@
 import pygame
-class TextBox(pygame.Surface)
-    def __init__(self, x, y, height, width, font = pygame.font.SysFont(pygame.font.get_default_font(), y - 10) , hint : Str = None, color_back = (255, 255, 255), color_font = (0, 0, 0)):
-        super.__init__((height, width))
+
+
+class TextBox(object):
+    def __init__(self, x, y, height, width, font=None, hint: str = None, color_back=(255, 255, 255), color_font=(0, 0, 0)):
         self.x = x
         self.y = y
         self.height = height
         self.width = width
+        self.color_back = color_back
+        self.color_font = color_font
         self.hint = hint
-        self.back_surface = pygame.Surface(width, height))
+        self.back_surface = pygame.Surface((width, height))
         self.back_surface.fill(color_back)
-        self.rect = pygame.Rect(x, y, TextBox.width, TextBox.height)
-        self.font = font
         self.text_surface = pygame.Surface((0, 0))
+        self.rect = pygame.Rect(x, y, width, height)
+        self.box_surface = self.back_surface = pygame.Surface((width, height))
+        if font is None:
+            self.font = pygame.font.SysFont(pygame.font.get_default_font(), self.y - 10)
+        else:
+            self.font = font
         self.text = ""
         self.active = False
 
@@ -26,10 +33,17 @@ class TextBox(pygame.Surface)
         if self.active:
             self.text += event.text
             self.text_surface = self.font.render(self.text, True, (0, 0, 0))
+            self.box_surface.blit(self.back_surface, (0, 0))
+            self.box_surface.blit(self.back_surface, (5, 5))
+
+    def draw(self):
+        return self.box_surface
 
     def render(self, screen):
-        screen.blit(self.back_surface, self.rect)
-        screen.blit(self.text_surface, (self.rect.x + 5, self.rect.y + 5))
+        screen.blit(self.text_surface, self.rect)
+        if self.active:
+            pygame.draw.line(screen, self.color_font, self.text_surface.get_rect().bottomright, self.text_surface.get_rect().topright)
+
 
 """
 tb = TextBox(100, 100)
